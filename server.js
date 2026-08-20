93334d5d0aced343038e3ecd3781b7e9a65c9667
OskuArt <mikromamzik@gmail.com>
2026-08-21 02:18:03 +0300

    Load V7 chat, meeting and achievement features

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const fs=require('fs');
 const path=require('path');
 const patchV6=require('./server-v6-patch');
 const patchChat=require('./server-chat-fix');
+const patchV7Features=require('./server-v7-features');
 try{
   const base=fs.readFileSync(path.join(__dirname,'server-base.js'),'utf8');
-  const generated=patchChat(patchV6(base));
+  const generated=patchV7Features(patchChat(patchV6(base)));
   const out=path.join(__dirname,'server.generated.js');
   fs.writeFileSync(out,generated);
   require(out);
