84f0f9e21c287351173a846285d73c2e49df6c64
OskuArt <mikromamzik@gmail.com>
2026-08-21 02:44:29 +0300

    Load permanent account deletion support

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const patchV6=require('./server-v6-patch');
 const patchChat=require('./server-chat-fix');
 const patchV7Features=require('./server-v7-features');
 const patchV8Features=require('./server-v8-features');
+const patchV9AccountDelete=require('./server-v9-account-delete');
 try{
   const base=fs.readFileSync(path.join(__dirname,'server-base.js'),'utf8');
-  const generated=patchV8Features(patchV7Features(patchChat(patchV6(base))));
+  const generated=patchV9AccountDelete(patchV8Features(patchV7Features(patchChat(patchV6(base)))));
   const out=path.join(__dirname,'server.generated.js');
   fs.writeFileSync(out,generated);
   require(out);
