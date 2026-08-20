ca251ce25c6662fc5cd191f990151ac0a54124c1
OskuArt <mikromamzik@gmail.com>
2026-08-21 02:35:21 +0300

    Load V8 rematch safeguards

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path=require('path');
 const patchV6=require('./server-v6-patch');
 const patchChat=require('./server-chat-fix');
 const patchV7Features=require('./server-v7-features');
+const patchV8Features=require('./server-v8-features');
 try{
   const base=fs.readFileSync(path.join(__dirname,'server-base.js'),'utf8');
-  const generated=patchV7Features(patchChat(patchV6(base)));
+  const generated=patchV8Features(patchV7Features(patchChat(patchV6(base))));
   const out=path.join(__dirname,'server.generated.js');
   fs.writeFileSync(out,generated);
   require(out);
