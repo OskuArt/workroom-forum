838be7e5dd599ec00cea2abc588477f96ad4e12a
OskuArt <mikromamzik@gmail.com>
2026-08-21 03:01:34 +0300

    Load V10 message actions

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const patchChat=require('./server-chat-fix');
 const patchV7Features=require('./server-v7-features');
 const patchV8Features=require('./server-v8-features');
 const patchV9AccountDelete=require('./server-v9-account-delete');
+const patchV10MessageActions=require('./server-v10-message-actions');
 try{
   const base=fs.readFileSync(path.join(__dirname,'server-base.js'),'utf8');
-  const generated=patchV9AccountDelete(patchV8Features(patchV7Features(patchChat(patchV6(base)))));
+  const generated=patchV10MessageActions(patchV9AccountDelete(patchV8Features(patchV7Features(patchChat(patchV6(base))))));
   const out=path.join(__dirname,'server.generated.js');
   fs.writeFileSync(out,generated);
   require(out);
